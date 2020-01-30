8e5a4d67467b3a5da1147d8c051cf12258b49f4d
treasurechristain <amadikwajoyn@gmail.com>
2020-01-30 14:33:49 -0800

    add prepared stmt

--- registration.php
+++ registration.php
@@ -1,10 +1,11 @@
 <?php
-	include_once('path/db.php');
+
+include_once('path/db.php');
 	if (isset($_POST['register'])) {
-	    $name = $_POST['fullname'];
-	    $email = $_POST['email'];
-	    $phone = $_POST['phone'];
-	    $password = $_POST['password'];
+	    $name = mysqli_real_escape_string($conn, $_POST['fullname']);
+	    $email = mysqli_real_escape_string($conn, $_POST['email']);
+	    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+	    $password = mysqli_real_escape_string($conn, $_POST['password']);
 	    $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
 
@@ -14,31 +15,125 @@
 	      if($numrow>0){
 	      echo "<script>alert('User Already Exists with Provided Details, Please use a Different Email and Phone Number.');</script>";
  }else{
- 	$sql = "INSERT INTO `profile` (`sn`, `pass_word`, `fullname`, `phone`, `email`, `gender`, `address`, `lga`, `state`, `nationality`,`image`, `next_of_kin_name`, `next_of_kin_phone`, `next_of_kin_address`, `next_of_kin_email`, `next_of_kin_relationship`, `spon_type`, `spon_name`, `spon_email`, `spon_address`, `spon_phone`) VALUES (NULL, '$password_hash','$name', '$phone', '$email', '', '', '', '', '','', '','','', '' ,'', '','','', '' ,'');";
-	if(mysqli_query($conn,$sql)){
-            $message = "User Registration Successful!";
-		// echo "User Registration Successful!";
-    }else{
-        $error = "User Registration not Successful. Try again later!";
-        // echo "User Registration not Successful. Try again later!";
-    }
-	if (isset($message)) {
-  		$sql = "SELECT * FROM `profile` WHERE `email` = '$email';";
-    	$query=mysqli_query($conn,$sql);
-      	$result=mysqli_fetch_array($query,MYSQLI_ASSOC);
-      	$id=$result['sn'];
-    
-    	echo "<script>alert('".$message."');</script>";
-    	$_SESSION['user'] = $id;
+	 $sql = "INSERT INTO `profile` (`pass_word`, `fullname`, `phone`, `email`, `gender`, `address`, `lga`, `state`, `nationality`,`image`, `next_of_kin_name`, `next_of_kin_phone`, `next_of_kin_address`, `next_of_kin_email`, `next_of_kin_relationship`, `spon_type`, `spon_name`, `spon_email`, `spon_address`, `spon_phone`) VALUES (?,?, ?, ?, '', '', '', '', '','', '','','', '' ,'', '','','', '' ,'');";
+	 
+	 $stmt = mysqli_stmt_init($conn);
+
+	if (!mysqli_stmt_prepare($stmt, $sql)) {
+		echo "Sql error";
+	} else {
+		mysqli_stmt_bind_param($stmt, "ssss", $password_hash, $name, $phone, $email);
+		mysqli_stmt_execute($stmt);
     	$_SESSION['fullname'] = $name;
     	$_SESSION['email'] = $email;
-    	echo "<script>window.location='index.php'</script>";
-	}elseif (isset($error)) {
-   		echo "<script>alert('".$error."');</script>";
+		echo "<script>window.location='index.php'</script>";
+	}
+}
 	}
 
-    }
-  }
+
+
+
+
+
+
+
+
+
+// 	if(mysqli_query($conn,$sql)){
+//             $message = "User Registration Successful!";
+//     }else{
+//         $error = "User Registration not Successful. Try again later!";
+        
+//     }
+// 	if (isset($message)) {
+//   		$sql = "SELECT * FROM `profile` WHERE `email` = '$email';";
+//     	$query=mysqli_query($conn,$sql);
+//       	$result=mysqli_fetch_array($query,MYSQLI_ASSOC);
+//       	$id=$result['sn'];
+    
+//     	echo "<script>alert('".$message."');</script>";
+//     	$_SESSION['user'] = $id;
+//     	$_SESSION['fullname'] = $name;
+//     	$_SESSION['email'] = $email;
+//     	echo "<script>window.location='index.php'</script>";
+// 	}elseif (isset($error)) {
+//    		echo "<script>alert('".$error."');</script>";
+// 	}
+
+//     }
+//   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 	include_once('path/db.php');
+// 	if (isset($_POST['register'])) {
+// 	    $name = mysqli_real_escape_string($conn, $_POST['fullname']);
+// 	    $email = mysqli_real_escape_string($conn, $_POST['email']);
+// 	    $phone = mysqli_real_escape_string($conn, $_POST['phone']);
+// 	    $password = mysqli_real_escape_string($conn, $_POST['password']);
+// 	    $password_hash = password_hash($password, PASSWORD_DEFAULT);
+
+
+// 	  $sql = "SELECT * FROM `profile` WHERE `email` = '$email' or `phone` = '$phone';";
+// 	   $query=mysqli_query($conn,$sql);
+// 	     $numrow=mysqli_num_rows($query);
+// 	      if($numrow>0){
+// 	      echo "<script>alert('User Already Exists with Provided Details, Please use a Different Email and Phone Number.');</script>";
+//  }else{
+//  	$sql = "INSERT INTO `profile` (`sn`, `pass_word`, `fullname`, `phone`, `email`, `gender`, `address`, `lga`, `state`, `nationality`,`image`, `next_of_kin_name`, `next_of_kin_phone`, `next_of_kin_address`, `next_of_kin_email`, `next_of_kin_relationship`, `spon_type`, `spon_name`, `spon_email`, `spon_address`, `spon_phone`) VALUES (NULL, '$password_hash','$name', '$phone', '$email', '', '', '', '', '','', '','','', '' ,'', '','','', '' ,'');";
+// 	if(mysqli_query($conn,$sql)){
+//             $message = "User Registration Successful!";
+//     }else{
+//         $error = "User Registration not Successful. Try again later!";
+        
+//     }
+// 	if (isset($message)) {
+//   		$sql = "SELECT * FROM `profile` WHERE `email` = '$email';";
+//     	$query=mysqli_query($conn,$sql);
+//       	$result=mysqli_fetch_array($query,MYSQLI_ASSOC);
+//       	$id=$result['sn'];
+    
+//     	echo "<script>alert('".$message."');</script>";
+//     	$_SESSION['user'] = $id;
+//     	$_SESSION['fullname'] = $name;
+//     	$_SESSION['email'] = $email;
+//     	echo "<script>window.location='index.php'</script>";
+// 	}elseif (isset($error)) {
+//    		echo "<script>alert('".$error."');</script>";
+// 	}
+
+//     }
+//   }
 ?>
 <!DOCTYPE html>
 <head>
